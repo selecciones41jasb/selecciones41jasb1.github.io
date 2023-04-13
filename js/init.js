06a35f5fe67f564d4fe5aa96da55d4e244d42c4e
selecciones41jasb <selecciones41jasb@gmail.com>
2023-04-12 18:18:46 -0600

    Cambio

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyAQqptto3q-jlL2Lf2WPIwaXyG0FfEg8zM",
+    authDomain: "selecciones41jasb2.firebaseapp.com",
+    projectId: "selecciones41jasb2",
+    storageBucket: "selecciones41jasb2.appspot.com",
+    messagingSenderId: "7340018491",
+    appId: "1:7340018491:web:a273d36d0e8064aae5451e",
+    measurementId: "G-M22D0HJ4KG"
+});
